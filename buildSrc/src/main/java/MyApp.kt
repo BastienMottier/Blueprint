e7203eed1b590530b3f2b1ef61e659a70145f77c
Unknown <bastien.mottier@gmail.com>
2020-07-05 14:35:42 +0200

    WIP

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "com.adraxas.squirclx"
-    const val version = 2000
-    const val versionName = "2.0.0"
+    const val version = 2105
+    const val versionName = "2.1.5"
 }
--- generate_icon_pack.php
+++ generate_icon_pack.php
@@ -5,7 +5,7 @@ function getAllIcons() {
 	$array = [];
 	foreach ($drawable as $an_icon) {
 		$an_icon = str_replace('./app/src/main/res/drawable-nodpi/', '', str_replace (".webp", '', $an_icon));
-		if (in_array($an_icon, ['clock_bg', 'clock_hour_hand', 'clock_minute_hand', 'app_logo', 'drawer_header', 'iconback', 'iconmask', 'ic_star'])) {
+		if (in_array($an_icon, ['clock_bg', 'clock_hour_hand', 'clock_minute_hand', 'app_logo', 'drawer_header', 'iconback', 'iconmask', 'ic_star', 'ic_launcher'])) {
 			continue;
 		}
 
@@ -13,6 +13,47 @@ function getAllIcons() {
 	}
 
 	return $array;
+}
+
+function getCategory() {
+    $jsoncategoy = file_get_contents('./app/categories.json');
+    $icons = explode("\n", file_get_contents('./app/src/main/res/xml/appfilter.xml'));
+    
+    $component_array = [];
+
+    if (!empty($jsoncategoy)) {
+        $component_array = json_decode($jsoncategoy, true);
+    }
+
+    foreach ($icons as $a_line) {
+        if (preg_match('@<item component="ComponentInfo\{(.*)\}"\s*(?:drawable|prefix)="([a-z0-9_]+)"\s*/>@i', $a_line, $match)) {
+            $package = explode('/', $match[1])[0];
+
+            if (empty($component_array[$match[2]]) || $component_array[$match[2]] == 404) {
+                $html = file_get_contents("https://play.google.com/store/apps/details?id=$package&hl=fr");
+
+                if (empty($html) || $html === 404) {
+                    $component_array[$match[2]] = 'no_playstore';
+
+                    continue;
+                }
+
+                preg_match ('@"applicationCategory":"([a-z0-9_\-]+)"@i', $html, $matches);
+
+                if (empty($matches)) {
+                    $component_array[$match[2]] = 'no_category';
+
+                    continue;
+                }
+
+                $component_array[$match[2]] = str_replace(' ', '_', strtolower($matches[1]));
+            }
+        }
+    }
+
+    file_put_contents('./app/categories.json', json_encode($component_array, JSON_PRETTY_PRINT));
+
+    return $component_array;
 }
 
 function getAllFromFile() {
@@ -36,10 +77,20 @@ function makeCategory($title, $icons) {
 	return $category;
 }
 
-
 $old_all = getAllFromFile();
 $new_all = getAllIcons();
 $new_icons = array_diff($new_all, $old_all);
+$category_by_icon = getCategory();
+
+$icon_by_category = [];
+
+foreach ($category_by_icon as $an_icon => $a_category) {
+    if (in_array($a_category, ['no_playstore', 'no_category'])) {
+        continue;
+    }
+    
+    $icon_by_category[$a_category][] = $an_icon;
+}
 
 $text = <<<EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -49,28 +100,15 @@ $text = <<<EOF
 EOF;
 
 $text .= makeCategory('icons_preview', $new_all);
-$text .= makeCategory('all', $new_all);
+$text .= makeCategory('all_icons', $new_all);
 $text .= makeCategory('recently_added', $new_icons);
-
-$files = glob('./app/categories/*');
-$categories_array = [
-	'all',
-	'recently_added'
-];
-
-foreach ($files as $a_file) {
-	$category_name = str_replace('./app/categories/', '', $a_file);
-
-	if ($category_name == 'all')
-		continue;
-	
-	$categories_array[] = $category_name;
-	$icons = explode("\n", file_get_contents($a_file));
-
-	$text .= makeCategory($category_name, $icons);
+ksort($icon_by_category);
+foreach ($icon_by_category as $a_category_titles => $icons) {
+    asort($icons);
+	$text .= makeCategory($a_category_titles, $icons);
 }
 
-$text .= makeCategory('icon_filters', $categories_array);
+$text .= makeCategory('icon_filters', array_merge(['all_icons', 'recently_added'], array_keys($icon_by_category)));
 
 $text .= "</resources>";
 
