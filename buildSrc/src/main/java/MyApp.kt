593577cccc03a8de2abc758015a929b59afa43c4
Unknown <bastien.mottier@gmail.com>
2020-07-13 07:38:02 +0200

    Last update

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "com.adraxas.squirclx"
-    const val version = 2105
-    const val versionName = "2.1.5"
+    const val version = 2106
+    const val versionName = "2.1.6"
 }
