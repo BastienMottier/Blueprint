5ab852e31b2997cd9836fe44be70f8ca11fbbd31
Bastien Mottier <bastien.mottier@gmail.com>
2021-01-08 20:09:32 +0100

    Last release

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "com.adraxas.squirclx"
-    const val version = 2108
-    const val versionName = "2.1.8"
+    const val version = 2200
+    const val versionName = "2.2.0"
 }
